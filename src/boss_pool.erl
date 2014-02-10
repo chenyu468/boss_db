2bc4d9fb96f8c0c5eeb6b617dcb19e3062354835
chenyu <chenyu@zenkore.com>
2014-02-10 08:33:21 +0800

    no message

--- src/boss_pool.erl
+++ src/boss_pool.erl
@@ -3,17 +3,17 @@
 -export([call/2, call/3]).
 
 call(Pool, Msg) ->
-    lager:info("boss_pool_call_1:~p,~p",[Pool,Msg]),
+    %% lager:info("boss_pool_call_1:~p,~p",[Pool,Msg]),
     Worker = poolboy:checkout(Pool),
     Reply = gen_server:call(Worker, Msg),
     poolboy:checkin(Pool, Worker),
-    lager:info("boss_pool_call_2"),
+    %% lager:info("boss_pool_call_2"),
     Reply.
 
 call(Pool, Msg, Timeout) ->
-    lager:info("boss_pool_call_4:~p,~p,~p",[Pool,Msg,Timeout]),
+    %% lager:info("boss_pool_call_4:~p,~p,~p",[Pool,Msg,Timeout]),
     Worker = poolboy:checkout(Pool),
     Reply = gen_server:call(Worker, Msg, Timeout),
     poolboy:checkin(Pool, Worker),
-    lager:info("boss_pool_call_5"),
+    %% lager:info("boss_pool_call_5"),
     Reply.
